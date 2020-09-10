ce96ba5751ca65fd99207b19bfaa96cecfec48ad
Lucas <lucasmart.tuc@gmail.com>
2020-09-10 16:45:59 -0300

    prefooter maquetado

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import Container from "react-bootstrap/Container";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import {MdEmail} from "react-icons/md"
+import "../../App.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Fragment>
+      <Container fluid className="pre-footer py-5 text-light">
+        <Row>
+          <Col className="text-center">
+            <h1>Suscribite al Newsletter</h1>
+            <p className="lead">
+              Recibi lo mejor de rollingNews en tu casilla de E-mail
+            </p>
+            <Container>
+              <div className="text-center">
+                <div className="d-flex justify-content-center">
+                    <Link className="text-dark pre-footer-btn"> <strong><MdEmail></MdEmail></strong> Quiero Suscribirme</Link>
+                </div>
+                <p className="mt-3 tyc"><i>Registrarse implica aceptar los Terminos y Condiciones</i></p>
+              </div>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+      <footer></footer>
+    </Fragment>
+  );
 };
 
 export default Footer;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import { FaRegPaperPlane } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavLink } from "react-router-dom";
@@ -15,72 +14,72 @@ const Header = () => {
     <Fragment>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand>
+          <NavLink exact={true} to="/" className="mr-2">
             <img
               src={logoEncabezado}
               width="200rem"
-              className=""
               alt="React Bootstrap logo"
             />
-          </Navbar.Brand>
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <NavLink
                 exact={true}
                 to="/categorias/actualidad"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Actualidad
               </NavLink>
               <NavLink
                 exact={true}
                 to="/categorias/tecnologia"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Tecnologia
               </NavLink>
               <NavLink
                 exact={true}
                 to="/categorias/politica"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Politica
               </NavLink>
               <NavLink
                 exact={true}
                 to="/categorias/economia"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Economia
               </NavLink>
               <NavLink
                 exact={true}
                 to="/categorias/deporte"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Deporte
               </NavLink>
               <NavLink
                 exact={true}
                 to="/categorias/espectaculo"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Espectaculo
               </NavLink>
-              <NavLink exact={true} to="/categorias/salud" className="nav-link">
+              <NavLink exact={true} to="/categorias/salud" className="rn-nav-link mx-2">
                 Salud
               </NavLink>
               <NavLink
                 exact={true}
                 to="/categorias/fotografia"
-                className="nav-link"
+                className="rn-nav-link mx-2"
               >
                 Fotografia
               </NavLink>
+              <div className="nav-line"></div>
             </Nav>
             <Nav className="ml-auto">
-              <NavDropdown title="Mi Cuenta" id="basic-nav-dropdown">
+              <NavDropdown title="Mi Cuenta" id="basic-nav-dropdown" className="rn-nav-link">
                 <NavDropdown.Item href="#action/3.1">Ingresar</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Registrarse
