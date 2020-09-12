35bb7e415e18b8b1aceb1fa2a8df9be08f986749
Lu Ghidara <lughidara@gmail.com>
2020-09-12 10:18:31 -0300

    maquetado sin colores

---
+++ src/components/Principal/BannerCovid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BannerCovid = () => {
+    return (
+        <div className="shadow border rounded p-3 mb-3">
+           <h1>BANNER COVID</h1> 
+        </div>
+    );
+};
+
+export default BannerCovid;
--- src/components/Principal/Inicio.js
+++ src/components/Principal/Inicio.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import ApiMoneda from './ApiMoneda';
+import UltimasNoticias from './UltimasNoticias'
+import ApiClima from './ApiClima';
+import NoticiasCategorias from './NoticiasCategorias';
+import BannerCovid from './BannerCovid';
 
 const Inicio = () => {
     return (
-        <div>
-            <h1>Pagina de Inicio</h1>
-        </div>
+        <section>
+            <div className="p-3 container my-3">
+                <div className="row d-flex justify-content-around">
+                    <div className="col-8">
+                        <UltimasNoticias></UltimasNoticias>
+                        <NoticiasCategorias className="shadow border rounded p-3"></NoticiasCategorias>
+                    </div>
+                    <div className="col-3">
+                        <ApiClima></ApiClima>
+                        <ApiMoneda></ApiMoneda>
+                    </div>
+                </div>
+                <section>
+                    <BannerCovid></BannerCovid>
+                </section>
+            </div>
+        </section>
     );
 };
 
