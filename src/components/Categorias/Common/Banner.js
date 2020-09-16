a5ed107ffa5b3bb50504dc8847477b132f9baa9f
Lucas <lucasmart.tuc@gmail.com>
2020-09-16 10:53:24 -0300

    banner categorias responsive

--- src/components/Categorias/Common/Banner.js
+++ src/components/Categorias/Common/Banner.js
@@ -7,7 +7,7 @@ import imgPrueba from "../../../img/Pruebas/prueba-01-01.jpg";
 const Banner = (props) => {
   return (
     <Fragment>
-      <div className="bg-naranja py-3">
+      <div className="bg-naranja py-3 d-none d-lg-block">
         <div className="formato-carousel">
           <h2 className="titulo text-center my-5">{props.titulo}</h2>
           <Carousel className="fc d-flex justify-content-center">
@@ -46,6 +46,51 @@ const Banner = (props) => {
               <Carousel.Caption>
                 <Link exact={true} to="/*" className="text-decoration-none">
                   <h2 className="titulo-carousel">Third slide label</h2>
+                </Link>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </div>
+      <div className="d-lg-none bg-naranja-sm">
+        <div className="formato-carousel container">
+          <h2 className="titulo my-4">{props.titulo}</h2>
+          <Carousel className="fc-sm d-flex justify-content-center">
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imgPrueba}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <Link exact={true} to="/*" className="text-decoration-none">
+                  <h2 className="titulo-carousel-sm">First slide label</h2>
+                </Link>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imgPrueba}
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <Link exact={true} to="/*" className="text-decoration-none">
+                  <h2 className="titulo-carousel-sm">Second slide label</h2>
+                </Link>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imgPrueba}
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <Link exact={true} to="/*" className="text-decoration-none">
+                  <h2 className="titulo-carousel-sm">Third slide label</h2>
                 </Link>
               </Carousel.Caption>
             </Carousel.Item>
