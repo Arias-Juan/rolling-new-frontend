acb4a466c9875f7aa75720088aaf45379ef20b5c
Lu Ghidara <lughidara@gmail.com>
2020-09-16 12:13:20 -0300

    maquetado pagina principal listo

--- src/components/Principal/Patrocinadores.js
+++ src/components/Principal/Patrocinadores.js
@@ -4,27 +4,39 @@ import { Link } from "react-router-dom";
 
 const Patrocinadores = () => {
     return (
-        <div className="mt-4">
-            <Link exact={true} to="/*" className="text-decoration-none"><img
-                className="d-block w-100 mb-3"
+        <div className="mt-4 patrocinadores">
+            <Link exact={true} to="/*" className="text-decoration-none">
+                <img
+                className="d-block w-100 mb-3 w-25"
                 src={`${process.env.PUBLIC_URL}Patrocinadores/publ_rc.png`}
-                alt="Rolling Code"
-            /></Link>
-            <Link exact={true} to="/*" className="text-decoration-none"><img
-                className="d-block w-100 mb-3"
+                alt="Rolling Code">
+
+                </img>
+            </Link>
+            <Link exact={true} to="/*" className="text-decoration-none">
+                <img
+                className="d-block w-100 mb-3 w-25"
                 src={`${process.env.PUBLIC_URL}Patrocinadores/publ_coca.png`}
-                alt="Coca-Cola"
-            /></Link>
-            <Link exact={true} to="/*" className="text-decoration-none"><img
-                className="d-block w-100 mb-3"
+                alt="Coca-Cola">
+
+                </img>
+            </Link>
+            <Link exact={true} to="/*" className="text-decoration-none">
+                <img
+                className="d-block w-100 mb-3 w-25"
                 src={`${process.env.PUBLIC_URL}Patrocinadores/publ_dell.png`}
-                alt="Dell"
-            /></Link>
-            <Link exact={true} to="/*" className="text-decoration-none"><img
+                alt="Dell">
+
+                </img>
+            </Link>
+            <Link exact={true} to="/*" className="text-decoration-none">
+                <img
                 className="d-block w-100 mb-3"
                 src={`${process.env.PUBLIC_URL}Patrocinadores/publ_google.png`}
-                alt="Google"
-            /></Link>
+                alt="Google">
+
+                </img>
+            </Link>
 
 
 
