97d14d486bb46dc93c2b662bc35a6c03d0698cf6
Lu Ghidara <lughidara@gmail.com>
2020-09-19 15:01:23 -0300

    antes de hacer pull

--- src/components/Categorias/Fotografia.js
+++ src/components/Categorias/Fotografia.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import Banner from "./Common/Banner";
+import React, { useState } from 'react';
+//import Banner from "./Common/Banner";
 //import Body from "./Common/Body";
 import PublicidadH from "../common/PublicidadH";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Link } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
 
 const Fotografia = (props) => {
 
@@ -16,27 +20,62 @@ const Fotografia = (props) => {
     return (itemNoticia.principalCategoria === "true" && itemNoticia.categoria === "fotografia");
   });
 
-  console.log(noticiasFotografia);
+  console.log(portada);
 
-    return (
-        <div className="mb-4">
-        {/* <Banner titulo={props.categorias[4]} noticias={portada}></Banner> */}
-        <hr></hr>
-        <div className="container-fluid">
-          <PublicidadH></PublicidadH>
-        </div>
-        <hr></hr>
-        <Container fluid>
-          <Row>
-            {noticiasFotografia.map((itemNoticiaFotografia) => 
-            <Col xs={12} md={6} lg={4} className="p-0 m-0">
-              <img src={itemNoticiaFotografia.imagenPrincipal} alt={itemNoticiaFotografia.descripcionBreve} className="w-100 p-1"></img>
-            </Col>).reverse()}
-            
-          </Row>
-        </Container>
+  //para la ventana modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className="mb-4">
+      {/* <Banner titulo={props.categorias[4]} noticias={portada}></Banner> */}
+      <div className="bg-naranja d-none d-lg-block m-0">
+        <h2 className="titulo text-center my-5">{props.categorias[4]}</h2>
       </div>
-    );
+      <hr></hr>
+      <div className="container-fluid">
+        <PublicidadH></PublicidadH>
+      </div>
+      <hr></hr>
+      <Container fluid>
+        <Row>
+          {noticiasFotografia.map((itemNoticiaFotografia) =>
+            <Col xs={12} md={6} lg={4} className="p-0 m-0">
+              <div><Link onClick={handleShow}>
+                <img src={itemNoticiaFotografia.imagenPrincipal} alt={itemNoticiaFotografia.descripcionBreve} className="w-100 p-1"></img>
+              </Link></div>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>
+                    {itemNoticiaFotografia.tituloNoticia}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <div className="text-center">
+                    <img src={itemNoticiaFotografia.imagenPrincipal} alt={itemNoticiaFotografia.descripcionBreve} className="w-100 p-1"></img>
+                  </div>
+                  {/* <div>
+                    <p className="font-weight-light">{itemNoticiaFotografia.descripcionDetallada}</p>
+                  </div> */}
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button
+                    variant="secondary"
+                    onClick={handleClose}
+                    className="rounded-pill"
+                  >
+                    Cerrar
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </Col>
+          ).reverse()}
+        </Row>
+      </Container>
+
+    </div>
+  );
 };
 
 export default Fotografia;
