d45b821078dcdb9491535007811d1175abaab19e
Lu Ghidara <lughidara@gmail.com>
2020-09-19 17:33:37 -0300

    solucionado un detalle en navbar administracion en Lista de Categorias

--- src/components/administracion/ListaCategoria.js
+++ src/components/administracion/ListaCategoria.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ListGroup from "react-bootstrap/ListGroup";
 import Categorias from './Categorias';
 import NavbarAdmin from "./common/NavbarAdmin";
 
 const ListaCategoria = (props) => {
   return (
-    <section className="container my-4">
+    <Fragment>
       <NavbarAdmin></NavbarAdmin>
-      <h1 className="text-center">Lista de Categorias</h1>
-      <ListGroup className="mb-5">
-        {props.categoriaNuevas.map((itemCategoria) => (
-          <Categorias key={itemCategoria.id} categoriaItem={itemCategoria}
-            setRecargarCategoria={props.setRecargarCategoria}></Categorias>
-        ))}
-      </ListGroup>
-    </section>
+      <section className="container my-4">
+        <h1 className="text-center">Lista de Categorias</h1>
+        <ListGroup className="mb-5">
+          {props.categoriaNuevas.map((itemCategoria) => (
+            <Categorias key={itemCategoria.id} categoriaItem={itemCategoria}
+              setRecargarCategoria={props.setRecargarCategoria}></Categorias>
+          ))}
+        </ListGroup>
+      </section>
+    </Fragment>
   );
 };
 
