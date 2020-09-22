35ae2b7d8258815b9d123e46eaff9ac1610fb586
Lu Ghidara <lughidara@gmail.com>
2020-09-22 19:09:54 -0300

    validaciones login

--- src/components/administracion/common/PrincipalAdmin.js
+++ src/components/administracion/common/PrincipalAdmin.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import NavbarAdmin from '../../administracion/common/NavbarAdmin';
 
 const PrincipalAdmin = () => {
     return (
         <div>
             <NavbarAdmin></NavbarAdmin>
-            <h1>principal Admin</h1>
+            <Container className='text-center'>
+                <div className="m-5 p-5">
+                    <h1 className="d-none d-sm-block">Bienvenido al ADMIN</h1>
+                    <h3 className="d-block d-sm-none">Bienvenido al ADMIN</h3>
+
+                </div>
+            </Container>
         </div>
     );
 };
