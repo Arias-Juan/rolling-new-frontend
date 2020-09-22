1f8b0056d706d3d69622f9f8e5318fb068e1b413
Lucas <lucasmart.tuc@gmail.com>
2020-09-22 10:17:30 -0300

    navbar fixeado en top

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
           (result) => {
             console.log(result.text);
             console.log(mailEnviado);
-            if(result.test === "OK"){
+            if (result.test === "OK") {
               setMailEnviado(true);
               setNombre("");
               setApellido("");
@@ -64,7 +64,9 @@ const Header = () => {
               setTelefono("");
               document.getElementById("form").reset();
               handleClose();
-              alert("Su suscripcion ha sido aceptada. Revisar casilla de correo.")
+              alert(
+                "Su suscripcion ha sido aceptada. Revisar casilla de correo."
+              );
             }
           },
           (error) => {
@@ -80,197 +82,224 @@ const Header = () => {
   };
   return (
     <Fragment>
-      <Navbar bg="light" expand="lg">
-        <Container className="divNavbar">
-          <NavLink exact={true} to="/" className="mr-2">
-            <img
-              src={logoEncabezado}
-              width="200rem"
-              alt="React Bootstrap logo"
-              className="logoEncabezado"
-            />
-          </NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <NavLink
-                exact={true}
-                to="/categorias/actualidad"
-                className="rn-nav-link mx-2"
-              >
-                Actualidad
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/tecnologia"
-                className="rn-nav-link mx-2"
-              >
-                Tecnologia
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/politica"
-                className="rn-nav-link mx-2"
-              >
-                Politica
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/economia"
-                className="rn-nav-link mx-2"
-              >
-                Economia
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/deportes"
-                className="rn-nav-link mx-2"
-              >
-                Deportes
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/espectaculos"
-                className="rn-nav-link mx-2"
-              >
-                Espectaculos
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/salud"
-                className="rn-nav-link mx-2"
-              >
-                Salud
-              </NavLink>
-              <NavLink
-                exact={true}
-                to="/categorias/fotografia"
-                className="rn-nav-link mx-2"
-              >
-                Fotografia
-              </NavLink>
-            </Nav>
-            <Nav className="ml-auto">
-              <NavDropdown
-                title="Mi Cuenta"
-                id="basic-nav-dropdown"
-                className="rn-nav-link"
-              >
-                <NavDropdown.Item href="/login">Ingresar</NavDropdown.Item>
-                <NavDropdown.Item href="/registro">
-                  Registrarse
-                </NavDropdown.Item>
-              </NavDropdown>
-              <button
-                className="btn-rn-outline ml-2"
-                onClick={handleShow}
-              >
-                <FaRegPaperPlane />
-              </button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <img
-              src={logoEncabezado}
-              width="200rem"
-              alt="React Bootstrap logo"
-            />
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="text-center">
-            <h4>Suscribite al Newsletter</h4>
-            <p>
-              Rellena el siguiente formulario y recibi en directo todas las
-              noticias en tu casilla de correo
-            </p>
-            <p className="color-naranja">
-              <i>(*)Campo Obligatorio</i>
-            </p>
-          </div>
-          {error ? ( //si el state error es verdadero
-            <Alert variant={"danger"}>Completar los datos obligatorios (*)</Alert>
-          ) : null}
-          {mailEnviado ? ( //si el state error es verdadero
-            <Alert variant={"success"}>Tu suscripcion ha sido registrada con exito!</Alert>
-          ) : null}
-          {mailError ? ( //si el state error es verdadero
-            <Alert variant={"warning"}>Ha ocurrido un error, por favor volver a intentarlo luego.</Alert>
-          ) : null}
-          <Form className="mb-4" onSubmit={handleSubmit} id="form">
-            <Form.Group controlId="formBasicEmail">
-              <Row>
-                <Col>
-                  <Form.Control placeholder="(*)Nombre" onChange={(e) => {setNombre(e.target.value)}}/>
-                </Col>
-                <Col>
-                  <Form.Control placeholder="(*)Apellido" onChange={(e) => {setApellido(e.target.value)}}/>
-                </Col>
-              </Row>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control type="text" placeholder="Direccion" />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Row>
-                <Col>
-                  <Form.Control placeholder="Codigo Postal" />
-                </Col>
-                <Col>
-                  <Form.Control placeholder="Localidad" />
-                </Col>
-              </Row>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control type="text" placeholder="(*)Telefono" onChange={(e) => {setTelefono(e.target.value)}}/>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control type="text" placeholder="(*)E-Mail" onChange={(e) => {setEmail(e.target.value)}}/>
-              <Form.Text className="text-muted">
-                Nosotros nunca compartiremos tus datos con nadie
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check
-                type="checkbox"
-                label="(*) Acepto los Terminos y Condiciones"
+      <div className="nb-margin">
+        <Navbar bg="light" expand="lg" fixed="top">
+          <Container className="divNavbar">
+            <NavLink exact={true} to="/" className="mr-2">
+              <img
+                src={logoEncabezado}
+                width="200rem"
+                alt="React Bootstrap logo"
+                className="logoEncabezado"
               />
-            </Form.Group>
+            </NavLink>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <NavLink
+                  exact={true}
+                  to="/categorias/actualidad"
+                  className="rn-nav-link mx-2"
+                >
+                  Actualidad
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/tecnologia"
+                  className="rn-nav-link mx-2"
+                >
+                  Tecnologia
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/politica"
+                  className="rn-nav-link mx-2"
+                >
+                  Politica
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/economia"
+                  className="rn-nav-link mx-2"
+                >
+                  Economia
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/deportes"
+                  className="rn-nav-link mx-2"
+                >
+                  Deportes
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/espectaculos"
+                  className="rn-nav-link mx-2"
+                >
+                  Espectaculos
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/salud"
+                  className="rn-nav-link mx-2"
+                >
+                  Salud
+                </NavLink>
+                <NavLink
+                  exact={true}
+                  to="/categorias/fotografia"
+                  className="rn-nav-link mx-2"
+                >
+                  Fotografia
+                </NavLink>
+              </Nav>
+              <Nav className="ml-auto">
+                <NavDropdown
+                  title="Mi Cuenta"
+                  id="basic-nav-dropdown"
+                  className="rn-nav-link"
+                >
+                  <NavDropdown.Item href="/login">Ingresar</NavDropdown.Item>
+                  <NavDropdown.Item href="/registro">
+                    Registrarse
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <button className="btn-rn-outline ml-2" onClick={handleShow}>
+                  <FaRegPaperPlane />
+                </button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <img
+                src={logoEncabezado}
+                width="200rem"
+                alt="React Bootstrap logo"
+              />
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
             <div className="text-center">
-              <Button type="submit" className="btn-rn rounded-pill">
-                Enviar
-              </Button>
+              <h4>Suscribite al Newsletter</h4>
+              <p>
+                Rellena el siguiente formulario y recibi en directo todas las
+                noticias en tu casilla de correo
+              </p>
+              <p className="color-naranja">
+                <i>(*)Campo Obligatorio</i>
+              </p>
             </div>
-          </Form>
-          <hr></hr>
-          <Link
-            className="btn btn-danger btn-block rounded-pill mt-4"
-            to="/*"
-          >
-            <FaGoogle/> &nbsp; Suscribirse con Google
-          </Link>
-          <Link
-            className="btn btn-primary btn-block rounded-pill my-4"
-            to="/*"
-          >
-            <FaFacebook/> &nbsp; Suscribirse con Facebook
-          </Link>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={handleClose}
-            className="rounded-pill"
-          >
-            Cerrar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+            {error ? ( //si el state error es verdadero
+              <Alert variant={"danger"}>
+                Completar los datos obligatorios (*)
+              </Alert>
+            ) : null}
+            {mailEnviado ? ( //si el state error es verdadero
+              <Alert variant={"success"}>
+                Tu suscripcion ha sido registrada con exito!
+              </Alert>
+            ) : null}
+            {mailError ? ( //si el state error es verdadero
+              <Alert variant={"warning"}>
+                Ha ocurrido un error, por favor volver a intentarlo luego.
+              </Alert>
+            ) : null}
+            <Form className="mb-4" onSubmit={handleSubmit} id="form">
+              <Form.Group controlId="formBasicEmail">
+                <Row>
+                  <Col>
+                    <Form.Control
+                      placeholder="(*)Nombre"
+                      onChange={(e) => {
+                        setNombre(e.target.value);
+                      }}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      placeholder="(*)Apellido"
+                      onChange={(e) => {
+                        setApellido(e.target.value);
+                      }}
+                    />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control type="text" placeholder="Direccion" />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Row>
+                  <Col>
+                    <Form.Control placeholder="Codigo Postal" />
+                  </Col>
+                  <Col>
+                    <Form.Control placeholder="Localidad" />
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  type="text"
+                  placeholder="(*)Telefono"
+                  onChange={(e) => {
+                    setTelefono(e.target.value);
+                  }}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Control
+                  type="text"
+                  placeholder="(*)E-Mail"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+                <Form.Text className="text-muted">
+                  Nosotros nunca compartiremos tus datos con nadie
+                </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check
+                  type="checkbox"
+                  label="(*) Acepto los Terminos y Condiciones"
+                />
+              </Form.Group>
+              <div className="text-center">
+                <Button type="submit" className="btn-rn rounded-pill">
+                  Enviar
+                </Button>
+              </div>
+            </Form>
+            <hr></hr>
+            <Link
+              className="btn btn-danger btn-block rounded-pill mt-4"
+              to="/*"
+            >
+              <FaGoogle /> &nbsp; Suscribirse con Google
+            </Link>
+            <Link
+              className="btn btn-primary btn-block rounded-pill my-4"
+              to="/*"
+            >
+              <FaFacebook /> &nbsp; Suscribirse con Facebook
+            </Link>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={handleClose}
+              className="rounded-pill"
+            >
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </Fragment>
   );
 };
